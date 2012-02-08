e67efef13e48cb348e2a8828edcd7f24b19b17a3
Jon Plaut <jp@tangerinesymphony.net>
2012-02-08 12:52:27 -0800

    first commit

---
+++ tasks.py
@@ -0,0 +1,80 @@
+from os import environ
+import pymongo
+import urllib
+import urllib2
+from collections import defaultdict
+import redis
+import pyres
+import simplejson as json
+from flask import Flask, request
+
+DBPATH=environ.get('MONGODBPATH')
+DBNAME=environ.get('MONGODBDATABASE')
+connection = pymongo.Connection(DBPATH)
+db = connection[DBNAME]
+
+redisHost = environ.get("REDIS_QUEUE_HOST")
+redisPort = int(environ.get("REDIS_QUEUE_PORT"))
+redisPassword = environ.get("REDIS_QUEUE_PASSWORD")
+
+redisObject = redis.Redis(host=redisHost, port=redisPort, password=redisPassword)
+
+redisQueue = pyres.ResQ(redisObject)
+
+def fql(fql, token, args=None):
+	if not args:
+		args = {}
+	
+	args["q"], args["format"], args["access_token"] = fql, "json", token
+
+	return json.loads(
+		urllib2.urlopen("https://graph.facebook.com/fql?" +
+						urllib.urlencode(args)).read())
+						
+def fb_call(call, args=None):
+	return json.loads(urllib2.urlopen("https://graph.facebook.com/" + call +
+									  '?' + urllib.urlencode(args)).read())	
+										
+class GetCheckinMetadata:
+	
+	queue = "get_checkin_metadata"
+		
+	@staticmethod
+	def perform(checkin_id, user, token):
+		checkin_metadata = {}
+		collection = db[user]
+		
+		checkin_metadata_all = fb_call(checkin_id, args={'access_token': token})
+		
+		if 'from' in checkin_metadata_all:
+			if 'name' in checkin_metadata_all['from']:
+				checkin_metadata['author_name'] = checkin_metadata_all['from']['name']
+			if 'id' in checkin_metadata_all['from']:
+				checkin_metadata['author_uid'] = checkin_metadata_all['from']['id']
+		if 'message' in checkin_metadata_all:
+			checkin_metadata['comment'] = checkin_metadata_all['message']
+		if 'place' in checkin_metadata_all:
+			if 'id' in checkin_metadata_all['place']:
+				checkin_metadata['place_id'] = checkin_metadata_all['place']['id']
+			if 'name' in checkin_metadata_all['place']:
+				checkin_metadata['place_name'] = checkin_metadata_all['place']['name']
+			if 'location' in checkin_metadata_all['place']:
+				checkin_metadata['place_city'] = checkin_metadata_all['place']['location']
+		
+		collection.insert(checkin_metadata)
+					
+class AggregateCheckins:
+	
+	queue = "aggregate_checkins"
+		
+	@staticmethod	
+	def perform(user, token, limit, offset):
+		
+		query1 = "\"query1\":\"SELECT uid2 FROM friend WHERE uid1=me() LIMIT %s OFFSET %s\"" % (limit, offset)
+		query2 = "\"query2\":\"SELECT checkin_id FROM checkin WHERE author_uid IN (SELECT uid2 FROM #query1)\""
+		
+		checkins = fql("{%s, %s}" % (query1, query2), token)['data'][1]['fql_result_set']
+		
+		for checkin in checkins:
+			redisQueue.enqueue(GetCheckinMetadata, str(checkin['checkin_id']), user, token)
+	
---
+++ tokens.py
@@ -0,0 +1,120 @@
+import base64
+import os
+import simplejson as json
+import urllib
+import urllib2
+
+from flask import Flask, request, redirect, url_for
+import redis
+import pyres
+from tasks import *
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+if os.environ.get('FACEBOOK_APP_ID'):
+	app.config.from_object('conf.Config')
+else:
+	app.config.from_envvar('MAIN_CONFIG')
+
+APP_ID = os.environ.get('FACEBOOK_APP_ID')
+APP_SECRET = os.environ.get('FACEBOOK_SECRET')
+
+redisHost = os.environ.get("REDIS_QUEUE_HOST")
+redisPort = int(os.environ.get("REDIS_QUEUE_PORT"))
+redisPassword = os.environ.get("REDIS_QUEUE_PASSWORD")
+
+redisObject = redis.Redis(host=redisHost, port=redisPort, password=redisPassword)
+
+redisQueue = pyres.ResQ(redisObject)
+
+def oauth_login_url(preserve_path=True, next_url=None):
+	fb_login_uri = ("https://www.facebook.com/dialog/oauth"
+					"?client_id=%s&redirect_uri=%s" %
+					(APP_ID, next_url))
+
+	if app.config['FBAPI_SCOPE']:
+		fb_login_uri += "&scope=%s" % ",".join(app.config['FBAPI_SCOPE'])
+	return fb_login_uri
+
+
+def simple_dict_serialisation(params):
+	return "&".join(map(lambda k: "%s=%s" % (k, params[k]), params.keys()))
+
+
+def base64_url_encode(data):
+	return base64.urlsafe_b64encode(data).rstrip('=')
+
+
+def fbapi_get_string(path, domain=u'graph', params=None, access_token=None,
+					 encode_func=urllib.urlencode):
+	"""Make an API call"""
+	if not params:
+		params = {}
+	params[u'method'] = u'GET'
+	if access_token:
+		params[u'access_token'] = access_token
+
+	for k, v in params.iteritems():
+		if hasattr(v, 'encode'):
+			params[k] = v.encode('utf-8')
+
+	url = u'https://' + domain + u'.facebook.com' + path
+	params_encoded = encode_func(params)
+	url = url + params_encoded
+	result = urllib2.urlopen(url).read()
+
+	return result
+
+
+def fbapi_auth(code):
+	params = {'client_id': APP_ID,
+			  'redirect_uri': "http://jp-checkin-tokens.herokuapp.com/callback/",
+			  'client_secret': APP_SECRET,
+			  'code': code}
+
+	result = fbapi_get_string(path=u"/oauth/access_token?", params=params,
+							  encode_func=simple_dict_serialisation)
+	pairs = result.split("&", 1)
+	result_dict = {}
+	for pair in pairs:
+		(key, value) = pair.split("=")
+		result_dict[key] = value
+	return (result_dict["access_token"], result_dict["expires"])
+	
+username = ''
+friendCount = 0
+limit = 20
+offset = 0
+requestsPerToken = 200
+
+	
+@app.route('/callback/', methods=['GET', 'POST'])
+def callback():
+	global offset
+	global username
+	global friendCount
+	
+	while offset <= friendCount:
+		if request.method == 'POST':
+			username = request.args.get('user')
+			friendCount = int(request.args.get('friends'))
+			token = fbapi_auth(request.args.get('code'))[0]
+			for i in xrange(0, requestsPerToken, limit):
+				redisQueue.enqueue(AggregateCheckins, username, token, limit, i)
+				offset += requestsPerToken
+			return redirect(oauth_login_url(next_url="http://jp-checkin-tokens.herokuapp.com/callback/"))
+		
+		if request.method == 'GET':
+			token = fbapi_auth(request.args.get('code'))[0]
+			for i in xrange(0, requestsPerToken, limit):
+				redisQueue.enqueue(AggregateCheckins, username, token, limit, i)
+				offset += requestsPerToken
+			return redirect(oauth_login_url(next_url="http://jp-checkin-tokens.herokuapp.com/callback/"))
+		
+if __name__ == '__main__':
+	port = int(os.environ.get("PORT", 5000))
+	if APP_ID and APP_SECRET:
+		app.run(host='0.0.0.0', port=port)
+	else:
+		print 'Cannot start application without Facebook App Id and Secret set'
